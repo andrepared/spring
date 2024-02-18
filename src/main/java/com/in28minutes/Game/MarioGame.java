8e47e9df83afb8d47b7c888ace9d75740a9b459e
Andres <par3des@gmail.com>
2024-02-17 22:50:31 -0500

    Creates AppGamingBasic, GameRunner, MarioGame

--- src/main/java/com/in28minutes/Game/MarioGame.java
+++ src/main/java/com/in28minutes/Game/MarioGame.java
@@ -1,4 +1,21 @@
 package com.in28minutes.Game;
 
-public class MarioGame {
+public class MarioGame
+{
+    public void up()
+    {
+        System.out.println("Jump");
+    }
+    public void down()
+    {
+        System.out.println("Go into a hole");
+    }
+    public void left()
+    {
+        System.out.println("Move left");
+    }
+    public void right()
+    {
+        System.out.println("Move right");
+    }
 }
